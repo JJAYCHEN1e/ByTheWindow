01876652b3905e6bf45170618b20383f30c3f93f
HavenTong <852092786@qq.com>
2020-02-16 16:05:40 +0800

    修改主界面

--- ByTheWindow/SceneDelegate.swift
+++ ByTheWindow/SceneDelegate.swift
@@ -20,9 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-//        let contentView = ContentView()
+        let contentView = ContentView()
 //        调试对对联界面
-        let contentView = MatchCoupletView()
+//        let contentView = MatchCoupletView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
