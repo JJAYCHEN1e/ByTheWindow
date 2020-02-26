013faa969ead338f70d437153fd4c3ffdf996170
mufanx <35735090+mufanx@users.noreply.github.com>
2020-02-26 17:26:36 +0800

    add uncomplete ARView of couplet

---
+++ ByTheWindow/ARCoupletView.swift
@@ -0,0 +1,30 @@
+//
+//  ARCoupletView.swift
+//  ByTheWindow
+//
+//  Created by 项慕凡 on 2020/2/26.
+//  Copyright © 2020 JJAYCHEN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ARCoupletView: View {
+    var body: some View {
+        ARCoupletView()
+    }
+}
+
+
+
+struct ARCoupletController: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ARCoupletController>) -> UIViewController {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let controller = storyboard.instantiateInitialViewController()
+        return controller!
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<ARCoupletController>) {
+        
+    }
+}
