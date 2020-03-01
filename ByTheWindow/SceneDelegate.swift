941f79094cb161aca0776e03b072884944964f7f
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2020-03-01 20:56:55 +0800

    Update SceneDelegate.swift

--- ByTheWindow/SceneDelegate.swift
+++ ByTheWindow/SceneDelegate.swift
@@ -29,7 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        调试对对联界面
 //        let contentView = MatchCoupletView()
         
-        let contentView = HomeView()
+//        let contentView = HomeView()
 //        let contentView = ARCoupletView()
 
         // Use a UIHostingController as window root view controller.
